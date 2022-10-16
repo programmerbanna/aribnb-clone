5213fe4d273aa08cd5226c73f08c4cf3b09d95cc
programmerbanna <programmerbanna@gmail.com>
2022-10-17 04:00:11 +0600

    tsconfig file re edited

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "JIT",
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

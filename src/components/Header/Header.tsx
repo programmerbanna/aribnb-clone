98f6a088b4c56d17012a139caad95cabee2c6764
programmerbanna <programmerbanna@gmail.com>
2022-10-19 05:07:28 +0600

    header done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import { GrSearch } from "react-icons/gr";
+import { RiGlobalLine } from "react-icons/ri";
+import { BiMenuAltRight } from "react-icons/bi";
+import { FaUserCircle } from "react-icons/fa";
 
 type HeaderProps = {};
 
@@ -13,12 +16,24 @@ const Header: React.FC<HeaderProps> = () => {
           layout="fill"
           objectFit="contain"
           objectPosition="left"
-          alt="aribnb"
+          alt="airbnb"
         />
       </div>
-      <div>
-        <input type="text" placeholder="Start your search..." />
-        <SearchIcon />
+      <div className="flex items-center py-2 rounded-full md:border-2">
+        <input
+          type="text"
+          placeholder="Start your search..."
+          className="flex-grow pl-5 text-gray-600 placeholder-gray-600 bg-transparent outline-none"
+        />
+        <GrSearch className="w-8 h-8 p-2 hidden md:inline-flex !text-white bg-red-400 rounded-full cursor-pointer md:mx-2" />
+      </div>
+      <div className="flex items-center justify-end space-x-4 text-gray-600">
+        <p className="hidden cursor-pointer md:inline">Become a host</p>
+        <RiGlobalLine className="h-6" />
+        <div className="flex items-center p-2 space-x-2 border-2 rounded-full">
+          <BiMenuAltRight className="h-6" />
+          <FaUserCircle className="h-6" />
+        </div>
       </div>
     </header>
   );

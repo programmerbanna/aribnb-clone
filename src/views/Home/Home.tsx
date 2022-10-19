f74e9f27889919ce40e9fbd0b9a2b2e1eb6c8713
programmerbanna <programmerbanna@gmail.com>
2022-10-20 03:57:59 +0600

    home banner added

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import Banner from "./components/Banner";
 
 type HomeProps = {};
 
 const Home: React.FC<HomeProps> = () => {
-  return <div>Have a good coding</div>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 export default Home;
--- src/views/Home/components/Banner.tsx
+++ src/views/Home/components/Banner.tsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
 import React from "react";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+      <Image
+        src="/images/home-banner.jpg"
+        layout="fill"
+        loading="lazy"
+        objectFit="cover"
+        alt="airbnb home banner"
+      />
+      <div className="absolute w-full text-center top-1/2">
+        <p className="text-sm sm:text-lg">
+          Fair data use and automated decision making.
+        </p>
+        <button className="px-10 py-4 my-3 font-bold text-purple-600 bg-white rounded-full shadow-md hover:shadow-xl">
+          Check here...
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
